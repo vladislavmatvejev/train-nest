eca77499dffc001a1e9fc52117dbece4a42c5fe0
Vladislav Matvejev <vladislavmatvejev13@gmail.com>
2024-01-20 12:24:23 +0200

    add pg package

---
+++ shipping-backend/ormconfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'your_username',
+    password: 'your_password',
+    database: 'your_database_name',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  };
+  
